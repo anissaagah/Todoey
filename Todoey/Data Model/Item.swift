527d87c09a73958b48aa46bce727657a4522cfdd
Anissa Agahchen <anissa@akimis-macbook-pro.local>
2018-06-14 17:07:52 -0700

    Convert to MVC Design Pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Anissa Agahchen on 2018-06-14.
+//  Copyright © 2018 Akimilabs. All rights reserved.
+//
+
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
+
